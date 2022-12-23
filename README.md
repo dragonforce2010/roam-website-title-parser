1f602bdc09a55ce16ad3820f567a2c0d4ffed26d
michael.zhang <michael.zhang@bytedance.com>
2022-12-24 02:34:45 +0800

    implement the first version of website title parser

---
+++ README.md
@@ -0,0 +1,6 @@
+# Roam Website Title Parser
+
+This extension can parse the website title and transform the link to the markdown format when you paste a url into roam block
+
+For example, when you paste a link like this `https://developer.roamjs.com/`, this extension is gonna transform it to [Introduction - RoamJS](https://developer.roamjs.com/)
+
---
+++ extension.js
@@ -0,0 +1,81 @@
+let currentUrlFromPaste = '';
+let serviceUrl = 'https://ec2-18-141-200-240.ap-southeast-1.compute.amazonaws.com'
+
+const pasteListener = (event) => {
+  const url = event.clipboardData.getData('text');
+  if (!CheckUrlFormat(url)) return;
+
+  currentUrlFromPaste = url;
+  console.log('get data from paste event: ', currentUrlFromPaste);
+
+  setTimeout(() => {
+    parseWebsiteUrlTitle(currentUrlFromPaste);
+  }, 800);
+};
+
+const CheckUrlFormat = (url) => {
+  const matchPattern =
+    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\*\+,;=.]+$/;
+  return matchPattern.test(url); // dummy
+};
+
+const parseWebsiteUrlTitle = async (url) => {
+  if (!url) return;
+
+  const resp = await fetch(`${serviceUrl}/parseWebsiteTitleFromUrl?url=${url}`);
+  const data = await resp.json();
+  const { websiteTitle } = data;
+  let urlWithMarkdownFormat = `[${websiteTitle}](${url})`;
+  updateCurrentBlockUrlFormat(urlWithMarkdownFormat);
+};
+
+
+const getBlockContent = (uid) => {
+  return window.roamAlphaAPI.q(`[:find (pull ?page [:block/string])
+                      :where [?page :block/uid "${uid}"]  ]`)[0][0].string;
+};
+
+const updateCurrentBlockUrlFormat = (urlWithMarkdownFormat) => {
+  let uid = window.roamAlphaAPI.ui.getFocusedBlock()?.['block-uid'];
+  console.log('get uid:', uid)
+  const originalContent = getBlockContent(uid);
+
+  // const replaceBegin = originalContent.indexOf(currentUrlFromPaste)
+  // const replaceEnd = originalContent.indexOf(currentUrlFromPaste) + currentUrlFromPaste.length
+  const newContent = originalContent.replaceAll(
+    currentUrlFromPaste,
+    urlWithMarkdownFormat
+  );
+  console.log(
+    'new content:', newContent
+  )
+
+  window.roamAlphaAPI.updateBlock({
+    block: { uid: uid, string: newContent },
+  });
+
+  setTimeout(() => {
+    document.activeElement.setSelectionRange(
+      document.activeElement.value.length,
+      document.activeElement.value.length
+    );
+  }, 100);
+
+  console.log('originalContent:', originalContent)};
+
+// actions that are predefined save there state automatically (except button) underneath the id provided for the action
+// custom actions can save state with extensionAPI.settings.set / get / getAll
+function onunload() {
+  window.removeEventListener('paste', pasteListener);
+}
+
+function onload({ extensionAPI, ...rest }) {
+  window.extensionAPI = extensionAPI;
+  console.log(extensionAPI, rest);
+  window.addEventListener('paste', pasteListener);
+}
+
+export default {
+  onload: onload,
+  onunload: onunload,
+};
